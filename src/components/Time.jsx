3a8f54c17704e131065a68997c1e65b841efb84d
Karen <karenchakhalyan23@gmail.com>
2021-03-01 00:24:10 +0400

    added some changes

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -2,20 +2,20 @@ import React ,{ useState } from "react";
 
 function Time() {
 
+    
     setInterval(getCurrentTime, 1000);
-
     const now = new Date().toLocaleTimeString();
     
     const [currentTime, setTime] = useState(now)
 
     function getCurrentTime(){
         const newTime = new Date().toLocaleTimeString();
-        setTime(newTime)
+        setTime(newTime)  
     }
 
     return <div>
         <h1>{currentTime}</h1>
-        <button onClick={getCurrentTime}>GET TIME</button>
+        <button onClick={getCurrentTime} className="btn">GET TIME</button>
     </div>
 }
 
